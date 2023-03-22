6ac08f2104d7a72e46793bc0518612f833cd148a
bezveze7 <128623893+bezveze7@users.noreply.github.com>
2023-03-22 18:02:55 +0100

    Create python3.sh

---
+++ python3.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+# Define the directory where the test files are located
+test_directory="/content/"
+# file path of the Python program
+python_file_path="SimEnka.py"
+
+
+num_tests_passed=0
+
+
+for test_dir in "$test_directory"/test*; do
+  echo "Running tests in $test_dir"
+
+
+  input_file="$test_dir/test.a"
+  expected_output_file="$test_dir/test.b"
+  actual_output_file="$test_dir/my_output.txt"
+
+
+  python3 "$python_file_path" < "$input_file" > "$actual_output_file"
+
+
+  if diff -q "$expected_output_file" "$actual_output_file" > /dev/null; then
+    echo "Test passed"
+    num_tests_passed=$((num_tests_passed+1))
+  else
+    echo "Test failed"
+  fi
+done
+echo "$num_tests_passed tests passed"
